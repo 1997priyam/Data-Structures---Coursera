a21db615b0997a2c10e1434cfe41fc5a60418a82
1997priyam <1997priyam@gmail.com>
2019-11-17 15:05:20 +0530

    Creating a heap out of a array

--- week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.cpp
+++ week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.cpp
@@ -13,6 +13,7 @@ class HeapBuilder {
  private:
   vector<int> data_;
   vector< pair<int, int> > swaps_;
+  int size = 0;
 
   void WriteResponse() const {
     cout << swaps_.size() << "\n";
@@ -24,9 +25,40 @@ class HeapBuilder {
   void ReadData() {
     int n;
     cin >> n;
+    size = n;
     data_.resize(n);
     for(int i = 0; i < n; ++i)
       cin >> data_[i];
+  }
+
+  int parent(int node){
+    return (node/2);
+  }
+
+  int leftChild(int node){
+    return (2*node);
+  }
+
+  int rightChild(int node){
+    return (2*node)+1;
+  }
+
+  void shiftDown(int num){
+    // int size = data_.size()-1;
+    int minIndex = num;
+    int l = leftChild(num);
+    int r = rightChild(num);
+    if(l<=size && data_[l-1]<data_[minIndex-1]){
+      minIndex = l;
+    }
+    if(r<=size && data_[r-1]<data_[minIndex-1]){
+      minIndex = r;
+    }
+    if(minIndex!=num){
+      swap(data_[minIndex-1], data_[num-1]);
+      swaps_.push_back(make_pair(num-1, minIndex-1));
+      shiftDown(minIndex);
+    }
   }
 
   void GenerateSwaps() {
@@ -38,13 +70,19 @@ class HeapBuilder {
     // but in the worst case gives a quadratic number of swaps.
     //
     // TODO: replace by a more efficient implementation
-    for (int i = 0; i < data_.size(); ++i)
-      for (int j = i + 1; j < data_.size(); ++j) {
-        if (data_[i] > data_[j]) {
-          swap(data_[i], data_[j]);
-          swaps_.push_back(make_pair(i, j));
-        }
-      }
+    // int size = data_.size()-1;
+    cout<<"The size of array is: "<<size<<std::endl;
+    for(int i=parent(size); i>=1; i--){
+      shiftDown(i);
+    }
+
+  //   for (int i = 0; i < data_.size(); ++i)
+  //     for (int j = i + 1; j < data_.size(); ++j) {
+  //       if (data_[i] > data_[j]) {
+  //         swap(data_[i], data_[j]);
+  //         swaps_.push_back(make_pair(i, j));
+  //       }
+  //     }
   }
 
  public:
